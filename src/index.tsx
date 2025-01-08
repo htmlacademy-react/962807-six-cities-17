b04f354e0720ce9c32549e885bc8fbe3c211453a
Yury <marchenko.yr@yandex.ru>
2025-01-08 20:53:12 +0300

    Подключение store приложению

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { OFFERS } from './mocks/offers';
 import { REVIEWS } from './mocks/reviews';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App offersCount={312} offers={OFFERS} reviews={REVIEWS} logged />
+    <Provider store={store}>
+      <App offersCount={312} offers={OFFERS} reviews={REVIEWS} logged />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/store/store.ts
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { reducer } from './reducer';
+
+export type State = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export const store = configureStore({ reducer });
