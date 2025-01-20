ab7cdb62d7d713f0a8e2c8e45553735ece3dd75e
Yury <marchenko.yr@yandex.ru>
2025-01-20 12:30:22 +0300

    Рефакторинг пустой main страницы

--- src/components/cities-empty/cities-empty.tsx
+++ src/components/cities-empty/cities-empty.tsx
@@ -1,9 +1,18 @@
-export default function CitiesEmpty(): JSX.Element {
+import { City } from '../../types';
+
+type CitiesEmptyProps = {
+  currentCity: City;
+};
+
+export default function CitiesEmpty({
+  currentCity,
+}: CitiesEmptyProps): JSX.Element {
   return (
     <div className="cities__status-wrapper tabs__content">
       <b className="cities__status">No places to stay available</b>
       <p className="cities__status-description">
-        We could not find any property available at the moment in Dusseldorf
+        {/* We could not find any property available at the moment in ${} */}
+        {`We could not find any property available at the moment in ${currentCity.name}`}
       </p>
     </div>
   );
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -61,7 +61,7 @@ export default function MainPage({
               }
             >
               {isEmpty() ? (
-                <CitiesEmpty />
+                <CitiesEmpty currentCity={currentCity} />
               ) : (
                 <CitiesPlaces
                   offersCount={getOffersCount()}
