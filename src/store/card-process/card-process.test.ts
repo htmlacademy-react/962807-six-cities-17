544240c96ed44a0d7de6d435907942233cd14f83
Yury <marchenko.yr@yandex.ru>
2025-01-27 16:30:03 +0300

    Старт модуль 9

---
+++ src/store/card-process/card-process.test.ts
@@ -0,0 +1,10 @@
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+
+describe('CardProcess Slice', () => {
+  it('simple test', () => {
+    const a = 0;
+    const b = 0;
+    expect(a).toEqual(b);
+  });
+});
